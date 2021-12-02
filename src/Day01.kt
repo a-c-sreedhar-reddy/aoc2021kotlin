d4de81205365c4c6161e0b0fbae1d0afcc6d2f5a
a-c-sreedhar-reddy <acreddysreedharreddy@gmail.com>
2021-12-03 01:29:12 +0530

    day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,37 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val measurements = input.map { measurement -> measurement.toInt() }
+        return measurements.foldIndexed(0) { index, acc, current ->
+            if (index === 0) {
+                acc
+            } else if (measurements[index - 1] <= current) {
+                acc + 1
+            } else {
+                acc
+            }
+        }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val measurements = input.map { measurement -> measurement.toInt() }
+
+        fun getLargeMeasurement(measurements: List<Int>, start: Int): Int {
+            return if (start + 3 >= measurements.size) {
+                0
+            } else {
+                val firstSum = measurements[start] + measurements[start + 1] + measurements[start + 2]
+                val secondSum = measurements[start + 1] + measurements[start + 2] + measurements[start + 3]
+                if (firstSum < secondSum) {
+                    1 + getLargeMeasurement(measurements, start + 1)
+                } else {
+                    getLargeMeasurement(measurements, start + 1)
+                }
+            }
+
+        }
+        return getLargeMeasurement(measurements, 0)
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
 
     val input = readInput("Day01")
     println(part1(input))
